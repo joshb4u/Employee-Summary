bfaa099477b075166459bd6c9e90e3ce6bc0acef
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-04-21 10:14:22 -0400

    Updated code in app.js and added output folder

--- app.js
+++ app.js
@@ -1,35 +1,142 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// Following External Modules are required - use 'npm install' in terminal
+const inquirer = require('inquirer')
+const path = require('path')
+const fs = require('fs')
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// Importing modules from lib folder
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const render = require('./lib/htmlRenderer')
 
-const render = require("./lib/htmlRenderer");
+// Output Path
+const OUTPUT_DIR = path.resolve(__dirname, 'output')
+const outputPath = path.join(OUTPUT_DIR, 'team.html')
 
+// Validating user inputs - string, email, number
+const validString = async input => (input !== '' ? true : 'Invalid input')
+const validEmail = input => (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input) ? true : 'Invalid input')
+const validNumber = async input => (input !== '' && !isNaN(input) ? true : 'Invalid input')
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Employee Questions prompted by the terminal
+const employeeQuestions = [
+    {
+        type: 'employeeInfo',
+        name: 'name',
+        message: 'Name:',
+        validate: validString
+    },
+    {
+        type: 'employeeInfo',
+        name: 'email',
+        message: 'Email:',
+        validate: validEmail
+    },
+    {
+        type: 'employeeInfo',
+        name: 'id',
+        message: 'ID:',
+        validate: validNumber
+    },
+]
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// Manager Questions prompted by the terminal
+const managerQuestions = [
+    {
+        type: 'employeeInfo',
+        name: 'officeNumber',
+        message: 'Office number:',
+        validate: validNumber
+    }
+]
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// Engineer Questions prompted by the terminal
+const engineerQuestions = [
+    {
+        type: 'employeeInfo',
+        name: 'github',
+        message: 'GitHub username:',
+        validate: validString
+    }
+]
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+// Intern Questions prompted by the terminal
+const internQuestions = [
+    {
+        type: 'employeeInfo',
+        name: 'school',
+        message: 'School attending:',
+        validate: validString
+    }
+]
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// Criteria for adding team members
+const nextTeamMember = [
+    {
+        type: 'list',
+        message: 'Which member would you like to add next to your team?',
+        name: 'next',
+        choices: ['Engineer', 'Intern', 'All Done'] //Manager is not included as every team is assumed to have only one manager
+    }
+]
+
+// Creating async function for awaiting prompts
+async function createTeam() {
+    
+    // Initialising team
+    const team = []
+    
+    // Beginning with Manager
+    let role = 'Manager'
+    
+    // Adding team members until user prompts 'All Done'
+    while (role != 'All Done') {
+        
+        // Getting info about employee
+        const employeeInfo = await inquirer.prompt(employeeQuestions)
+        
+        // Adding role specific information and creating team member
+        let specialInfo
+        let newTeamMember
+        
+        switch (role) {
+            
+            case 'Manager':
+            specialInfo = await inquirer.prompt(managerQuestions)
+            newTeamMember = new Manager(employeeInfo.name,employeeInfo.email,employeeInfo.id,specialInfo.officeNumber)
+            break
+            
+            case 'Engineer':
+            specialInfo = await inquirer.prompt(engineerQuestions)
+            newTeamMember = new Engineer(employeeInfo.name,employeeInfo.email,employeeInfo.id,specialInfo.github)
+            break
+            
+            case 'Intern':
+            specialInfo = await inquirer.prompt(internQuestions)
+            newTeamMember = new Intern(employeeInfo.name,employeeInfo.email,employeeInfo.id,specialInfo.school)
+            break
+            
+            default:
+            console.log('Error')
+        }
+        
+        // Adding next team member 
+        team.push(newTeamMember)
+        
+        // Setting role of next member
+        const nextMember = await inquirer.prompt(nextTeamMember)
+        role = nextMember.next
+    }
+    
+    // HTML rendering: team.html
+    const html = render(team)
+    
+    // Creating HTML file
+    fs.writeFile(outputPath, html, err => {
+        if (err) throw err
+        console.log(`File ${path.basename(outputPath)} is saved.`)
+    })
+}
+
+// Calling async function
+createTeam()
